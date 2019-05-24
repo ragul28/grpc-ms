238390e86957234954a3909e16b18c0078280823
Ragulan <ragulh28@gmail.com>
2019-05-24 15:04:00 +0530

    token-service with jwt encode & decode

---
+++ user-service/token_service.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+
+	pb "github.com/grpc-ms/user-service/proto/user"
+)
+
+var (
+	key = []byte("r8a28fa553c0fb97c61699a3dfbbe20e")
+)
+
+type CustomClaims struct {
+	User *pb.User
+	jwt.StandardClaims
+}
+
+type Authable interface {
+	Decode(token string) (*CustomClaims, error)
+	Encode(user *pb.User) (string, error)
+}
+
+type TokenService struct {
+	repo repository
+}
+
+// Decode token into token-object
+func (s *TokenService) Decode(tokenString string) (*CustomClaims, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (s *TokenService) Encode(user *pb.User) (string, error) {
+
+	expireToken := time.Now().Add(time.Hour * 72).Unix()
+
+	claims := CustomClaims{
+		user,
+		jwt.StandardClaims{
+			ExpiresAt: expireToken,
+			Issuer:    "gomicro.user.service",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(key)
+}
