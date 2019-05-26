5e8932e2241be98eb595f52f7f2110daa5e545b5
Ragulan <ragulh28@gmail.com>
2019-05-26 20:43:37 +0530

    Helper func for getenv & code cleanup

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -7,16 +7,60 @@ import (
 	"net"
 	"os"
 
-	pb "github.com/grpc-ms/vessel-service/proto/vessel"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	pb "github.com/grpc-ms/vessel-service/proto/vessel"
 )
 
 const (
-	port        = ":50052"
-	defaultHost = "localhost:27017"
+	Port   = ":50052"
+	DBHost = "mongodb://localhost:27017"
 )
 
+func main() {
+
+	Port := getEnv("GRPC_PORT", Port)
+	DBHost := getEnv("DB_HOST", DBHost)
+
+	lis, err := net.Listen("tcp", Port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	//create mongodb client
+	client, err := CreateClient(DBHost)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB connected at", DBHost)
+	defer client.Disconnect(context.TODO())
+
+	vesselCollection := client.Database("grpc-ms").Collection("vessel")
+	repository := &VesselRepository{
+		vesselCollection,
+	}
+
+	createDummyData(repository)
+	// checkFindAv(repository)
+	pb.RegisterVesselServiceServer(s, &handler{repository})
+
+	reflection.Register(s)
+
+	log.Println("Running on port:", Port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+//Ceate dummy vessl data
 func createDummyData(repo repository) {
 	vessels := []*pb.Vessel{
 		{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
@@ -26,6 +70,7 @@ func createDummyData(repo repository) {
 	}
 }
 
+// Debug function for Findav Vessel
 func checkFindAv(repo repository) {
 
 	vesselRes, err := repo.FindAvailable(&pb.Specification{
@@ -39,45 +84,11 @@ func checkFindAv(repo repository) {
 	}
 }
 
-func main() {
-
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+// Getenv Helper func
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
 	}
-	s := grpc.NewServer()
-
-	uri := os.Getenv("DB_HOST")
-	if uri == "" {
-		uri = "mongodb://" + defaultHost
-	}
-
-	client, err := CreateClient(uri)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("DB connected at", uri)
-	defer client.Disconnect(context.TODO())
-
-	vesselCollection := client.Database("grpc-ms").Collection("vessel")
-	repository := &VesselRepository{
-		vesselCollection,
-	}
-
-	createDummyData(repository)
-	//checkFindAv(repository)
-	pb.RegisterVesselServiceServer(s, &handler{repository})
-
-	reflection.Register(s)
-
-	log.Println("Running on port:", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	return value
 }
