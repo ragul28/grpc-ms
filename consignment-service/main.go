47e9230d30d695d871c2fb938efc60cc76ba4db8
Ragulan <ragulh28@gmail.com>
2019-05-26 21:01:05 +0530

    Removed gomicro >added authInterceptor  & getEnv

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -3,94 +3,125 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
+	"net"
 	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
 
 	// Import the generated protobuf code
 	pb "github.com/grpc-ms/consignment-service/proto/consignment"
 	userProto "github.com/grpc-ms/user-service/proto/user"
 	vesselProto "github.com/grpc-ms/vessel-service/proto/vessel"
-	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/metadata"
-	"github.com/micro/go-micro/server"
 )
 
 const (
-	defaultHost = "localhost:27017"
+	Port          = ":50051"
+	DBHost        = "mongodb://localhost:27017"
+	VesselAddress = "localhost:50052"
+	UserAddress   = "localhost:50053"
 )
 
 func main() {
-	// setup go-micro
-	srv := micro.NewService(
-		micro.Name("gomicro.consignment.service"),
-		micro.Version("latest"),
-		//auth middleware
-		micro.WrapHandler(AuthWrapper),
-	)
 
-	srv.Init()
+	Port := getEnv("GRPC_PORT", Port)
+	DBHost := getEnv("DB_HOST", DBHost)
+	VesselAddress := getEnv("VESSEL_HOST", "localhost:50052")
 
-	//Get database host
-	uri := os.Getenv("DB_HOST")
-	if uri == "" {
-		uri = "mongodb://" + defaultHost
+	lis, err := net.Listen("tcp", Port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
 	}
 
+	s := grpc.NewServer(
+		// GRPC server with auth Interceptor
+		grpc.UnaryInterceptor(AuthInterceptor),
+	)
+
 	//create mongodb client session
-	session, err := createClient(uri)
+	session, err := createClient(DBHost)
 	if err != nil {
 		log.Panic(err)
 	}
-
-	log.Println("DB connected at", uri)
+	log.Println("DB connected at", DBHost)
 	defer session.Disconnect(context.TODO())
 
 	consignmentCollection := session.Database("grpc-ms").Collection("consignments")
-
 	repository := &MongoRepository{consignmentCollection}
-	vesselClient := vesselProto.NewVesselServiceClient("gomicro.vessel.service", srv.Client())
-	h := &handler{repository, vesselClient}
+
+	// connection to vessel client via grpc
+	conn, err := grpc.Dial(VesselAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	vesselClient := vesselProto.NewVesselServiceClient(conn)
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), h)
+	pb.RegisterShippingServiceServer(s, &handler{repository, vesselClient})
 
-	//Run server
-	if err := srv.Run(); err != nil {
-		fmt.Println(err)
+	reflection.Register(s)
+
+	//Run grpc server
+	log.Println("Running on port:", Port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
 //Auth middleware to validate token in consignment svc api
-func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
-	return func(ctx context.Context, req server.Request, resp interface{}) error {
-		//To skip auth for dev
-		DISABLE_AUTH := false
-		if os.Getenv("DISABLE_AUTH") == "true" || DISABLE_AUTH {
-			return fn(ctx, req, resp)
-		}
+func AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 
-		meta, ok := metadata.FromContext(ctx)
-		if !ok {
-			return errors.New("no auth meta-data found in request")
-		}
+	DisableAuth := getEnv("DISABLE_AUTH", "false")
+	UserAddress := getEnv("USER_HOST", "localhost:50053")
 
-		token := meta["Token"]
-		log.Println("Authenticating with token: ", token)
-
-		authClient := userProto.NewUserServiceClient("gomicro.user.service", client.DefaultClient)
-		authResp, err := authClient.ValidateToken(ctx, &userProto.Token{
-			Token: token,
-		})
-		log.Println("Auth resp:", authResp)
-
-		if err != nil {
-			log.Println("Err:", err)
-			return err
-		}
-
-		err = fn(ctx, req, resp)
-		return err
+	//To skip auth for dev
+	if DisableAuth == "true" {
+		log.Println("skipping the token auth", DisableAuth)
+		return handler(ctx, req)
 	}
+
+	// Check incoming context for metadata for jwt token
+	meta, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, errors.New("no auth meta-data found in request")
+	}
+
+	token := meta["token"][0]
+	log.Println("Authenticating with token: ", token)
+	// authResp, err := TokeValidate(token)
+
+	// Set up a connection to the user server
+	conn, err := grpc.Dial(UserAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := userProto.NewUserServiceClient(conn)
+	authResp, err := c.ValidateToken(ctx, &userProto.Token{Token: token})
+	log.Println("Auth resp:", authResp)
+	if err != nil {
+		log.Fatalf("could not authenticate: %v", err)
+	}
+
+	return handler(ctx, req)
+}
+
+// Getenv Helper func
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
+// Debug function for token validation
+func TokeValidate(token string) (bool, error) {
+	if len(token) < 1 {
+		return false, errors.New("error missing token")
+	}
+	return token == "secret-token", nil
 }
